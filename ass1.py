7b9db2f17cf4361f46e97a8768b633e5d85e2e9e
codethorr <149942822+codethorr@users.noreply.github.com>
2023-11-06 11:22:33 +0530

    Add files via upload

---
+++ ass1.py
@@ -0,0 +1,25 @@
+import time
+
+def fiboIter(n):
+    prevNum = 0
+    currentNum = 1
+    for i in range(1, n):
+        prevPrevNum = prevNum
+        prevNum = currentNum
+        currentNum = prevNum + prevPrevNum
+    return currentNum
+
+def fiboRecur(n):
+    if n==0:
+        return 0
+    elif n==1:
+        return 1
+    else:
+        return fiboRecur(n-1) + fiboRecur(n-2)
+    
+if __name__ == "__main__":
+    num = int(input("Enter a number: "))
+    init = time.time()
+    print(f"Using recursions value of fib({num}) is {fiboRecur(num)}")
+    print(f"Using iterations value of fib({num}) is {fiboIter(num)}")
+    print(f"It took {time.time() - init} seconds")
